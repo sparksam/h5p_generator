9e8e9a759e637fb25bb730c24a0abee53ad9808f
Samuel Klutse <klutse.samuel@gmail.com>
2023-02-10 12:48:58 -0500

    Possibility to zip unzip and preview h5p content

--- generator.py
+++
@@ -1,9 +0,0 @@
-
-def h5p_zipper(destination_dir:str, filename:str):
-    raise NotImplementedError("Should implement the method to zip the archive")
-
-def h5p_unzipper(source_dir:str, filename:str):
-    raise NotImplementedError("Should implement the method to unzip the archive")
-
-if __name__ == '__main__': 
-    print("Hello World")
