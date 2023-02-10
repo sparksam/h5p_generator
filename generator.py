8877567c58725013f2d03265b7f799d1ccd093bb
Samuel Klutse <klutse.samuel@gmail.com>
2023-02-10 10:43:12 -0500

    Initial commit

---
+++ generator.py
@@ -0,0 +1,9 @@
+
+def h5p_zipper(destination_dir:str, filename:str):
+    raise NotImplementedError("Should implement the method to zip the archive")
+
+def h5p_unzipper(source_dir:str, filename:str):
+    raise NotImplementedError("Should implement the method to unzip the archive")
+
+if __name__ == '__main__': 
+    print("Hello World")
